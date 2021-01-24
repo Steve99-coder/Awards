0ce0790fa11e6d8c7b0e2d9bc016cc7dc789265c
Steve99-coder <stevenderitu99@gmail.com>
2021-01-24 09:53:34 +0300

    Add a function for new project

--- appawards/views.py
+++ appawards/views.py
@@ -15,3 +15,22 @@ def welcome(request):
         average=(project.design + project.usability + project.content)/3
         rating = round(average,2)
     return render(request, 'users/index.html', {'user_profile':user_profile, 'projects':projects,'comment':comment})
+
+
+@login_required(login_url='/accounts/login/')
+def new_project(request):
+    current_user = request.user
+  
+    if request.method == 'POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user = current_user
+            project.save()
+
+        return redirect ("welcome")
+
+    else:
+        form = NewProjectForm()
+
+    return render(request, 'users/new_project.html', {"form": form})
