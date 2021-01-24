c2f0ac049b2d19486e7a5c35310e56b6af868589
Steve99-coder <stevenderitu99@gmail.com>
2021-01-24 09:50:01 +0300

    Add the model for project

--- appawards/models.py
+++ appawards/models.py
@@ -14,7 +14,6 @@ class Profile(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE, blank=True)
     email=models.EmailField()
     phone_number = models.CharField(max_length =30,blank =True)
-    # user_project=models.ForeignKey(Project, on_delete=models.CASCADE)
     @classmethod
     def update_profile(cls,id,value):
         cls.objects.filter(id=id).update(user_id = new_user)
@@ -28,3 +27,47 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user
+
+
+class Project(models.Model):
+
+    title = models.CharField(max_length =60)
+    image=models.ImageField(upload_to = 'pic/', blank=False, null=False)
+    description=HTMLField()
+    link=models.URLField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE,)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True, null=True)
+    comments=models.CharField(max_length=100, blank=True)
+    design=models.IntegerField(choices=list(zip(range(0,11), range(0,11))),default=0)
+    usability=models.IntegerField(choices=list(zip(range(0,11), range(0,11))),default=0)
+    content=models.IntegerField(choices=list(zip(range(0,11), range(0,11))),default=0)
+    vote=models.IntegerField(default=0)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def update_description(cls,id,description):
+        description=cls.objects.filter(description_id=id).update(description=description)
+        
+        return description
+
+    @classmethod
+    def get_projects(cls):
+        projects=cls.objects.all().prefetch_related('comment_set')
+        return projects
+
+    @classmethod
+    def search_project(cls,title):
+        title = cls.objects.filter(title__icontains=title)
+        return title
+
+
