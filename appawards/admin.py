cf403b61464bb95f866e0ac43c6174b97b8a9b8b
Steve99-coder <stevenderitu99@gmail.com>
2021-01-24 10:17:07 +0300

    Add imports to admin.py

--- appawards/admin.py
+++ appawards/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from .models import User
 
 # Register your models here.
