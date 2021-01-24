4fb779853103da9f9ec76324c9fd8d634368161d
Steve99-coder <stevenderitu99@gmail.com>
2021-01-24 10:15:51 +0300

    Add url patterns in the app

--- appawards/urls.py
+++ appawards/urls.py
@@ -7,3 +7,16 @@ from django.contrib.auth.models import User
 import datetime as dt
 from tinymce.models import HTMLField
 from django.contrib.auth import views as auth_views
+
+urlpatterns=[
+    url(r'^profile$', views.user_profile, name='user-profile'),
+    url(r'^$', views.welcome, name="welcome"),
+    url(r'^edit/profile$', views.edit_profile, name="edit-profile"),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^vote/(?P<id>\d+)', views.rating,name='rating'),
+    url(r'^newcomment/(\d+)/$', views.new_comment, name='new-comment'),
+    url(r'^new/project$',views.new_project, name ='new-project'),
+    url(r'^api/profile/$', views.ProfileList.as_view(), name='profile-API'),
+    url(r'^api/project/$', views.ProjectList.as_view(), name='project-API'),
+    
+]
