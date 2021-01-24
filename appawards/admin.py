bfa28703b5ff9ab921d1b4c43de317131120bb52
Steve99-coder <stevenderitu99@gmail.com>
2021-01-24 10:18:31 +0300

    Add a class for user creation form

--- appawards/admin.py
+++ appawards/admin.py
@@ -8,3 +8,16 @@ from .models import User
 class UserAdmin(AuthUserAdmin):
     add_form = MyUserCreationForm
     update_form_class = UserChangeForm
+
+class MyUserCreationForm(UserCreationForm):
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        try:
+            User._default_manager.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
