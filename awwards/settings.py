dccf120b4ca26ba69a10996dad9c17c07740f149
Steve99-coder <stevenderitu99@gmail.com>
2021-01-24 09:44:40 +0300

    Add the installed app in the settings

--- awwards/settings.py
+++ awwards/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appawards',
+    'tinymce',
+    'rest_framework',
+    'bootstrap4',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
