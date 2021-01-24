feaac5fbf6587cba3fdf3bf4b006977b2f166ed6
Steve99-coder <stevenderitu99@gmail.com>
2021-01-24 10:11:19 +0300

    Add aclaa for new profile form

--- appawards/forms.py
+++ appawards/forms.py
@@ -2,3 +2,9 @@ from django import forms
 from .models import Profile,Project,Comment
 from django.contrib.auth.models import User
 from django.forms import ModelForm
+
+
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model= Profile
+        exclude =['user']
