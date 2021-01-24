df18f6afa0cb7acb22211e0da43af8d1f7d0bb9f
Steve99-coder <stevenderitu99@gmail.com>
2021-01-24 11:00:22 +0300

    Add a test for commwnt

--- appawards/tests.py
+++ appawards/tests.py
@@ -27,5 +27,32 @@ class ProjectTestClass(TestCase):
     def test_delete_project(self):
         self.new_project.delete_project()
         self.assertEqual(len(Project.objects.all()),0)
+
+class CommentTestClass(TestCase):
+    def setUp(self):
+
+        self.new_user=User(id=1,username='Steve')
+        self.new_project=Project(id=2,title='moringa',image='pic.jpg', user=self.new_user, description='Absolutely perfect',vote=2,)
+        self.new_comment=Comment(id=3,comment='Did great',user=self.new_user, project=self.new_project)
+
+    def test_instance(self): 
         
+        self.assertTrue(isinstance(self.new_user,User))
+        self.assertTrue(isinstance(self.new_project,Project))
+        self.assertTrue(isinstance(self.new_comment,Comment))
+
+
+    def test_save_comment(self):
+        self.new_comment.save_comment()
+        comments=Comment.objects.all()
+        self.assertTrue(len(comments)>0)
+
+    def tearDown(self):
+        Comment.objects.all().delete()
+
+    def test_delete_comment(self):
+        self.new_comment.delete_comment()
+        self.assertEqual(len(Comment.objects.all()),0)
+ 
+
  
