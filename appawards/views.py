d1d622e1cb9630d9f7a6cebcd58dd966df37ad2f
Steve99-coder <stevenderitu99@gmail.com>
2021-01-24 09:52:42 +0300

    Add a function for welcome

--- appawards/views.py
+++ appawards/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Profile,Project,Comment
+import datetime as dt
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def welcome(request):
+    current_user = request.user
+    user_profile= Profile.objects.filter(user=current_user.id).first()
+    comment= Comment.objects.filter(user=current_user.id).first()
+    projects = Project.objects.all()
+    average=0
+
+    for project in projects:
+        average=(project.design + project.usability + project.content)/3
+        rating = round(average,2)
+    return render(request, 'users/index.html', {'user_profile':user_profile, 'projects':projects,'comment':comment})
